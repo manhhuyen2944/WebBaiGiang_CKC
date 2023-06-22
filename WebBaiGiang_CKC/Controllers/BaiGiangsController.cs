e3656de6b15f17b4950fbb9de4a4e36b39bb739a
khangpm20a <0306201451@caothang.edu.vn>
2023-06-22 21:35:45 +0700

    chuc nang bai kiem tra + cap nhap bai lam + Luu thong tin + lay cau hoi + ramdom tao de ngau nhien

--- WebBaiGiang_CKC/Controllers/BaiGiangsController.cs
+++ WebBaiGiang_CKC/Controllers/BaiGiangsController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using WebBaiGiang_CKC.Data;
+using WebBaiGiang_CKC.Models;
+using X.PagedList;
 
 namespace WebBaiGiang_CKC.Controllers
 {
@@ -82,6 +84,86 @@ namespace WebBaiGiang_CKC.Controllers
 
             return View();
         }
+        public IActionResult KyThi()
+        {
+            var kikiemtra = _context.DanhSachThi.Include(t => t.KyKiemTra).Include(x => x.TaiKhoan).ToList();
+            ViewBag.kiemtra = kikiemtra;
+            return View();
+        }
 
+        [Route("/BaiGiangs/KyThi/BaiKiemTra")]
+        [HttpPost]
+        public async Task<IActionResult> BaiKiemTra(int id, int? page)
+        {
+            var mssvclaim = User.Claims.FirstOrDefault(c => c.Type == "MSSV");
+            var mssv_ = "";
+            if (mssvclaim != null)
+            {
+                mssv_ = mssvclaim.Value;
+            }
+            // lấu câu hỏi đã chọn và ramdom
+            var random = new Random();
+            var ds_cauhoi = _context.CauHoi_De.Where(x => x.De.KyKiemTra.KyKiemTraId == id).Include(t => t.CauHoi).Include(t => t.De).ThenInclude(t => t.KyKiemTra).AsEnumerable().OrderBy(x => random.Next()).ToList();
+            // trung vấn bài làm & kiểm tra điều kiệm mssv , 
+            var kiemtrabailam = await _context.CauHoi_BaiLam.Include(x => x.BaiLam).FirstOrDefaultAsync(x => x.BaiLam.MSSV == mssv_ && x.CauHoi_De.De.KyKiemTraId == id);
+            //tìm kiếm kiều kiện để add dữ liệu tại câu trên truy vấn bảng chưa có dữ liệu
+            var thoigiandenhan = await _context.CauHoi_De.FirstOrDefaultAsync(x => x.De.KyKiemTraId == id);
+            var tenkikiem = "";
+            tenkikiem = thoigiandenhan.De.KyKiemTra.TenKyKiemTra;
+            if (ModelState.IsValid)
+            {
+                // Kiểm tra xem đã thêm 'BaiLam' cho 'De' này chưa
+                DateTime currentTime = DateTime.UtcNow.AddHours(7);
+                DateTime startDateTime = new DateTime(currentTime.Year, currentTime.Month, currentTime.Day, currentTime.Hour, currentTime.Minute, currentTime.Second);
+                DateTime updatedStartDateTime = startDateTime.AddMinutes(thoigiandenhan.De.KyKiemTra.ThoiGianLamBai);
+                if (kiemtrabailam == null)
+                {
+                    var newbai = new BaiLam
+                    {
+                        MSSV = mssv_,
+                        HoTen = User.Identity.Name,
+                        ThoiGianBatDau = startDateTime,
+                        ThoiGianDenHan = (updatedStartDateTime < thoigiandenhan.De.KyKiemTra.ThoiGianKetThuc) ? updatedStartDateTime : thoigiandenhan.De.KyKiemTra.ThoiGianKetThuc,
+                    };
+                    _context.BaiLam.Add(newbai);
+                    await _context.SaveChangesAsync();
+                    // 
+                    var baiLamId = newbai.BaiLamId;
+                    // add bang cauhoi_bailam
+                    var cauHoiBaiLamListb = ds_cauhoi.Select(x => new CauHoi_BaiLam
+                    {
+                        BaiLamId = baiLamId,
+                        CauHoi_DeId = x.CauHoi_DeId
+                    }).ToList();
+                    _context.CauHoi_BaiLam.AddRange(cauHoiBaiLamListb);
+                    await _context.SaveChangesAsync();
+                }
+            }
+            var newexistingBaiLam = await _context.CauHoi_BaiLam.FirstOrDefaultAsync(x => x.BaiLam.MSSV == mssv_ && x.CauHoi_De.De.KyKiemTraId == id);
+            ViewBag.kiemtrasv_id = newexistingBaiLam.BaiLam.MSSV;
+            ViewBag.TenKiKiemTra = tenkikiem;
+            ViewBag.IdKiKiemTra = id;
+            // thời gian đếm ngược khi làm bài tự nộp 
+            var tgbd = DateTime.Now;
+            var tg_kt = newexistingBaiLam.BaiLam.ThoiGianDenHan;
+            TimeSpan timeSpan = (DateTime)tg_kt - (DateTime)tgbd;
+            ViewBag.TimThoigian = timeSpan;
+            ///thoi gian lam bai cua sv truy vấn ở đây chờ ở trên add dữ liệu mới có mà sài 
+            var cauhoi_de_mssv = await _context.CauHoi_BaiLam.Where(x => x.BaiLam.MSSV == mssv_ && x.CauHoi_De.De.KyKiemTraId == id).Include(x => x.BaiLam).Include(x => x.CauHoi_De).ToListAsync();
+            ViewBag.cauhoi_de_mssv = cauhoi_de_mssv;
+            DateTime? tg_batdau = newexistingBaiLam.BaiLam.ThoiGianBatDau;
+            DateTime? tg_ketthuc = newexistingBaiLam.BaiLam.ThoiGianDenHan;
+            ViewBag.tg_batdau = tg_batdau;
+            ViewBag.tg_ketthuc = tg_ketthuc;
+
+
+            var pageNo = page == null || page <= 0 ? 1 : page.Value;
+            var pageSize = 1;
+            PagedList<CauHoi_BaiLam> models = new PagedList<CauHoi_BaiLam>(cauhoi_de_mssv, pageNo, pageSize);
+            ViewBag.CurrentPage = pageNo;
+
+            return View(models);
+
+        }
     }
 }
