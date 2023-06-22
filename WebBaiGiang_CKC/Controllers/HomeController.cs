e252b6e74c5e8f4ec8ae0f60b894a985cedfff1f
khangpm20a <0306201451@caothang.edu.vn>
2023-06-22 21:22:04 +0700

    cap nhap model

--- WebBaiGiang_CKC/Controllers/HomeController.cs
+++ WebBaiGiang_CKC/Controllers/HomeController.cs
@@ -1,20 +1,46 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using WebBaiGiang_CKC.Data;
 using WebBaiGiang_CKC.Models;
 
 namespace WebBaiGiang_CKC.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+
+        protected WebBaiGiangContext _context;
+
+        protected readonly IWebHostEnvironment _environment;
+
+        public INotyfService _notyfService { get; }
+        public HomeController(WebBaiGiangContext context, IWebHostEnvironment environment, INotyfService notyfService)
         {
-            _logger = logger;
+            _context = context;
+            _environment = environment;
+            _notyfService = notyfService;
         }
+
 
         public IActionResult Index()
         {
+            var mssvclaim = User.Claims.FirstOrDefault(c => c.Type == "MSSV");
+            var mssv_ = "";
+            if (mssvclaim != null)
+            {
+                mssv_ = mssvclaim.Value;
+            }
+
+            var kikiemtra = _context.DanhSachThi.Include(t => t.KyKiemTra).Include(x => x.TaiKhoan).ToList();
+            DanhSachThi kiemtra_ = null;
+            foreach (var kiemtra in kikiemtra)
+            {
+                kiemtra_ = _context.DanhSachThi.FirstOrDefault(x => x.TrangThai == false && x.TaiKhoan.MSSV == mssv_ && x.KyKiemTraId == kiemtra.KyKiemTraId);
+            }
+            ViewBag.kikiemtra = kiemtra_;
             return View();
         }
 
@@ -28,5 +54,28 @@ namespace WebBaiGiang_CKC.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+
+            var lstMon = _context.MonHoc.Include(a => a.Chuongs).ThenInclude(x => x.Bais).AsNoTracking().ToList();
+            ViewData["lstSubject"] = lstMon;
+            var mssvclaim = User.Claims.FirstOrDefault(c => c.Type == "MSSV");
+            var mssv_ = "";
+            if (mssvclaim != null)
+            {
+                mssv_ = mssvclaim.Value;
+            }
+
+            var kikiemtra = _context.DanhSachThi.Include(t => t.KyKiemTra).Include(x => x.TaiKhoan).ToList();
+            DanhSachThi kiemtra_ = null;
+            foreach (var kiemtra in kikiemtra)
+            {
+                kiemtra_ = _context.DanhSachThi.FirstOrDefault(x => x.TrangThai == false && x.TaiKhoan.MSSV == mssv_ && x.KyKiemTraId == kiemtra.KyKiemTraId);
+            }
+            ViewBag.kikiemtra = kiemtra_;
+            base.OnActionExecuting(filterContext);
+        }
+
     }
 }
