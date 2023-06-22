8f48c3f8fb2eb4779fcdb9a5dc18adf463d4d622
khangpm20a <0306201451@caothang.edu.vn>
2023-06-22 21:28:13 +0700

    cap nhap de cuong

---
+++ WebBaiGiang_CKC/Controllers/BaiGiangsController.cs
@@ -0,0 +1,19 @@
+﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+using WebBaiGiang_CKC.Data;
+
+namespace WebBaiGiang_CKC.Controllers
+{
+    public class BaiGiangsController : HomeController
+    {
+        public BaiGiangsController(WebBaiGiangContext context, IWebHostEnvironment environment, INotyfService notyfService)
+            : base(context, environment, notyfService)
+        {
+
+        }
+        public IActionResult DeCuong()
+        {
+            return View();
+        }
+    }
+}
