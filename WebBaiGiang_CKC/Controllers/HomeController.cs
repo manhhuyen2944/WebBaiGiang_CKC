0ec86a2418a7ee1da8063ebf46fbaf5d86e96647
khangpm20a <0306201451@caothang.edu.vn>
2023-06-26 00:08:33 +0700

    cap nhap bai kiem tra + admin quan ly tai khoan

--- WebBaiGiang_CKC/Controllers/HomeController.cs
+++ WebBaiGiang_CKC/Controllers/HomeController.cs
@@ -35,10 +35,10 @@ namespace WebBaiGiang_CKC.Controllers
             }
 
             var kikiemtra = _context.DanhSachThi.Include(t => t.KyKiemTra).Include(x => x.TaiKhoan).ToList();
-            DanhSachThi kiemtra_ = null;
+            var kiemtra_ = 0;
             foreach (var kiemtra in kikiemtra)
             {
-                kiemtra_ = _context.DanhSachThi.FirstOrDefault(x => x.TrangThai == false && x.TaiKhoan.MSSV == mssv_ && x.KyKiemTraId == kiemtra.KyKiemTraId);
+                kiemtra_ = _context.KyKiemTra.Include(x => x.DanhSachThi).Count(x => x.DanhSachThi.First().TaiKhoan.MSSV == mssv_ && x.DanhSachThi.First().TrangThai == false && x.KyKiemTraId == kiemtra.KyKiemTraId && DateTime.UtcNow.AddHours(7) >= x.ThoiGianBatDau && DateTime.UtcNow.AddHours(7) < x.ThoiGianKetThuc);
             }
             ViewBag.kikiemtra = kiemtra_;
             return View();
@@ -68,11 +68,11 @@ namespace WebBaiGiang_CKC.Controllers
             }
 
             var kikiemtra = _context.DanhSachThi.Include(t => t.KyKiemTra).Include(x => x.TaiKhoan).ToList();
-            DanhSachThi kiemtra_ = null;
+            var kiemtra_ = 0;
             CauHoi_BaiLam diemsv = null;
             foreach (var kiemtra in kikiemtra)
             {
-                kiemtra_ = _context.DanhSachThi.FirstOrDefault(x => x.TrangThai == false && x.TaiKhoan.MSSV == mssv_ && x.KyKiemTraId == kiemtra.KyKiemTraId);
+                kiemtra_ = _context.KyKiemTra.Include(x => x.DanhSachThi).Count(x =>  x.DanhSachThi.First().TaiKhoan.MSSV == mssv_ && x.DanhSachThi.First().TrangThai == false && x.KyKiemTraId == kiemtra.KyKiemTraId && DateTime.UtcNow.AddHours(7) >= x.ThoiGianBatDau && DateTime.UtcNow.AddHours(7) < x.ThoiGianKetThuc);
                 diemsv = _context.CauHoi_BaiLam.Include(x => x.BaiLam).Include(x => x.CauHoi_De).ThenInclude(x => x.De).FirstOrDefault(x=>x.CauHoi_De.De.KyKiemTraId == kiemtra.KyKiemTraId && x.BaiLam.MSSV == mssv_);
             }
             ViewBag.diemsv = diemsv;
